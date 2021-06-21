d4f1083ab3d45976cc09a76570b2dc9b34ca758d
unknown <bnsviv@gmail.com>
2021-06-21 16:56:36 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+Pseudo-random generator
+There are many areas where we need random data.
+
+One of them is testing. 
+We may need random data: text, numbers, etc. to test things out well.
+
+In JavaScript, we could use Math.random(). 
+But if something goes wrong, we’d like to be able to repeat 
+the test, using exactly the same data.
+
+For that, so called “seeded pseudo-random generators” are used. 
+They take a “seed”, the first value, and then generate the 
+next ones using a formula so that the same seed yields the 
+same sequence, and hence the whole flow is easily reproducible. 
+We only need to remember the seed to repeat it.
+
+An example of such formula, that generates somewhat uniformly 
+distributed values:
+
+next = previous * 16807 % 2147483647
+
+If we use 1 as the seed, the values will be:
+
+1. 16807
+2. 282475249
+3. 1622650073
+…and so on…
+The task is to create a generator function pseudoRandom(seed) that takes seed and creates the generator with this formula.
+
+Usage example:
+
+let generator = pseudoRandom(1);
+
+alert(generator.next().value); // 16807
+alert(generator.next().value); // 282475249
+alert(generator.next().value); // 1622650073
+
+## attempt at solution
+
+function* generateBySeed() {
+	let value = seed;
+
+	while(true) {
+		value = value * 16807 % 2147483647
+		yield value;
+	}
+
+};
+
+let generator = generateBySeed(1);
+
+alert(generator()); // 16807
+alert(generator()); // 282475249
+alert(generator()); // 1622650073
+
+That also works. But then we lose ability to iterate with for..of 
+and to use generator composition, that may be useful elsewhere.
